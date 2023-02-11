ec9f767559d2c74a1ebda8cf3df1d968cbb70e89
Arpit Sharma <s.arpit9656@gmail.com>
2023-02-12 00:55:09 +0530

    bug fix

--- mouth_movement.py
+++ mouth_movement.py
@@ -5,7 +5,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 from statistics import median
 
-path = "/Users/arpitsharma/Downloads/slow_traffic_small.mp4"
+path = "/Users/arpitsharma/Downloads/Video.mp4"
 model = "/Users/arpitsharma/Downloads/face_detection_yunet_2022mar.onnx"
 
 def visualize(input, faces, thickness=2):
@@ -29,7 +29,7 @@ def gaussian(left_r=-80, right_r=80, sigma=75):
 
     return y
 
-cap = cv.VideoCapture(0)
+cap = cv.VideoCapture(path)
 print("[INFO] Opening camera...")
 time.sleep(1.0)
 if not cap.isOpened():
@@ -59,7 +59,7 @@ print("[INFO] Starting video...")
 
 while True:
     ret, frame = cap.read()
-
+    frameCounter += 1
     if not ret:
         print('No frames grabbed!')
         break
@@ -73,45 +73,47 @@ while True:
     detector.setInputSize((frame.shape[1], frame.shape[0]))
     faces = detector.detect(frame)
     # print(faces)
+    ratio = 0
+    if faces[1] is not None:
+        for face in faces[1]:
+            coords = face[:-1].astype(np.int32)
+            (x, y, w, h) = (coords[0], coords[1], coords[2], coords[3] + (coords[3] * 0.05).astype(np.int32))
 
-    for face in faces[1]:
-        coords = face[:-1].astype(np.int32)
-        (x, y, w, h) = (coords[0], coords[1], coords[2], coords[3])
-        (startX, startY, endX, endY) = (x, y, x + w, y + h)
+            (startX, startY, endX, endY) = (x, y, x + w, y + h)
 
-        # check detect_face for returning value
+            # check detect_face for returning value
 
-        cv.rectangle(frame, (startX, startY), (endX, endY), (255, 255, 255), 2)
-        heat_map = np.zeros_like(frame[startY:endY, startX:endX])
-        heat_map[..., 1] = 0
-        heat_map[..., 0] = 0
-        heat_map[..., 2] = cv.normalize(abs(fy[startY:endY, startX:endX]), None, 0, 255, cv.NORM_MINMAX)
+            cv.rectangle(frame, (startX, startY), (endX, endY), (255, 255, 255), 2)
+            heat_map = np.zeros_like(frame[startY:endY, startX:endX])
+            heat_map[..., 1] = 0
+            heat_map[..., 0] = 0
+            heat_map[..., 2] = cv.normalize(abs(fy[startY:endY, startX:endX]), None, 0, 255, cv.NORM_MINMAX)
 
-        m = heat_map[..., 2]
-        image = cv.resize(m, (120, 160), interpolation=cv.INTER_CUBIC)
-        (h, w) = image.shape
-        y_vertical = gaussian(left_r=-60, right_r=60, sigma=30)
-        y_horizontal = gaussian(left_r=-50, right_r=30, sigma=20)
+            m = heat_map[..., 2]
+            image = cv.resize(m, (120, 160), interpolation=cv.INTER_CUBIC)
+            (h, w) = image.shape
+            y_vertical = gaussian(left_r=-60, right_r=60, sigma=30)
+            y_horizontal = gaussian(left_r=-50, right_r=30, sigma=20)
 
-        score2 = np.sum(y_vertical * image[0:int(h / 2), 0:w].T)
-        score1 = np.sum(y_vertical * image[int(h / 2):h, 0:w].T)
+            score2 = np.sum(y_vertical * image[0:int(h / 2), 0:w].T)
+            score1 = np.sum(y_vertical * image[int(h / 2):h, 0:w].T)
 
-        score2 += np.sum(y_horizontal * image[0:int(h / 2), 0:w])
-        score1 += np.sum(y_horizontal * image[int(h / 2):h, 0:w])
+            score2 += np.sum(y_horizontal * image[0:int(h / 2), 0:w])
+            score1 += np.sum(y_horizontal * image[int(h / 2):h, 0:w])
 
-        print(min(15, score1 / score2), score2, score1)
-        cv.imshow('image', image)
-        ratio = min(15, score1 / score2)
-        fps = cap.get(cv.CAP_PROP_FPS)
+            # print(min(15, score1 / score2), score2, score1)
+            cv.imshow('image', image)
+            ratio = min(15, score1 / score2)
+            fps = cap.get(cv.CAP_PROP_FPS)
 
-        activity.append(ratio)
-        if frameCounter % 10 == 0:
-            X.append(frameCounter)
-            avg.append(sum(activity[-int(fps + 10):]) / (fps + 10))
-            med.append(median(activity[-int(fps + 10):]))
+    activity.append(ratio)
+    if frameCounter % 10 == 0:
+        X.append(frameCounter)
+        avg.append(sum(activity[-int(10):]) / (10))
+        med.append(median(activity[-int(10):]))
 
-        if sum(activity[-int(fps + 10):]) > fps * 3:
-            cv.circle(frame, (x, y), 5, (0, 0, 255), -1)
+    if sum(activity[-int(10):]) > 30:
+        cv.circle(frame, (x, y), 5, (0, 0, 255), -1)
 
     info = "No. of Frames : {}".format(frameCounter)
     cv.putText(frame, info, (10, frame.shape[0]-60), font, 0.5, (255, 255, 255), 1, cv.LINE_AA)
